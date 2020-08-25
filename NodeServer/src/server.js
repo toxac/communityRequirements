0d3171c1461668ff93d8a57bbc91c7bdc3e51598
ac <derocon@gmail.com>
2020-08-25 16:18:35 +0530

    server file

---
+++ NodeServer/src/server.js
@@ -0,0 +1,9 @@
+/*
+  description: Entry point for  node server for commexchange
+  stack : express, mongoDB
+  @author: Amit Chanchal
+  @date: 25-August-2020
+  @version: 0.0.1
+*/
+
+const express = require('express');
